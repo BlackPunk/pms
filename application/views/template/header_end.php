60a52097b0ff9d24018bfd9e8937fe9c2433aea6
BlackPunk <blackpunk1337@gmail.com>
2020-04-27 04:01:32 +0700

    Done

--- application/views/template/header_end.php
+++ application/views/template/header_end.php
@@ -1,13 +1,11 @@
-<<<<<<< HEAD:application/views/template/header_end.php
+<!-- CSS file -->
+<link rel="stylesheet" href="<?= base_url('assets/css/'); ?>bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/'); ?>all.min.css" type="text/css">
+<link rel="stylesheet" href="<?= base_url('assets/css/'); ?>style.css" type="text/css">
 
-    <!-- CSS file -->
-    <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>all.min.css" type="text/css">
-    <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>style.css" type="text/css">
-
-    <!-- JS file -->
-    <script defer src="<?= base_url('assets/js/') ?>all.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<!-- JS file -->
+<script defer src="<?= base_url('assets/js/') ?>all.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -16,23 +14,4 @@
         <div id="status">
             <div class="spinner"></div>
         </div>
-=======
-
-    <!-- CSS file -->
-    <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>all.min.css" type="text/css">
-    <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>style.css" type="text/css">
-
-    <!-- JS file -->
-    <script defer src="<?= base_url('assets/js/') ?>all.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-</head>
-
-<body>
-    <!-- Loader -->
-    <div id="preloader">
-        <div id="status">
-            <div class="spinner"></div>
-        </div>
->>>>>>> 4e4975501622401e0e43227541651ea2d24f84e0:application/views/template/header.php
     </div>
--- application/views/template/header_start.php
+++ application/views/template/header_start.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
@@ -12,19 +11,4 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <title><?= $title ?> - Sistem manajemen pasien</title>
 
     <!-- Icon -->
-=======
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $title ?></title>
-
-    <!-- Icon -->
->>>>>>> 4e4975501622401e0e43227541651ea2d24f84e0
     <link rel="shortcut icon" href="<?= base_url('assets/images/'); ?>favicon.ico" type="image/x-icon">
